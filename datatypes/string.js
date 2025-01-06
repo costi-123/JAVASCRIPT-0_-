aeba1e3300a781757c15c433b839e4b68571222e
costi-123 <costiyadav@gmail.com>
2025-01-06 16:44:04 +0530

    string

---
+++ datatypes/string.js
@@ -0,0 +1,11 @@
+let str = String("vardhan") // declaring string as object
+// console.log(`${str}, yes it is my name`); // string manupulation
+
+// for (let i = 0; i < str.length; i++) {
+//     const element = str[i];
+//     console.log(element.toUpperCase());
+    
+    
+// }
+console.log(str.charAt(2));
+
--- datatypes/tempCodeRunnerFile.js
+++ datatypes/tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-)
-// console.log("02" == 2); // here "02" (string) is converted into 2(number)
+v
