6e28fc0cec9f41486c6c73df1215aa8f8244578a
costi-123 <costiyadav@gmail.com>
2025-01-18 20:05:50 +0530

    all i don't know

--- fetch/script.js
+++ fetch/script.js
@@ -1,4 +1,4 @@
-fetch("https://jsoplaceholder.typicode.com/users")
+fetch("https://jsonplaceholder.typicode.com/users")
 .then(function (response) {
     return response.json();
 }).then((response) => {
